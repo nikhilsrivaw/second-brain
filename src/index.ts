a0e667d4d7b9f1b739b3ffabe3f3b90f19cd49c2
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2025-05-26 17:16:11 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+
+import express from 'express';
+
+const app = express();
+
+
+
+app.post('/api/v1/signup' , (req,res)=>{
+
+})
+app.post('/api/v1/signin' , (req,res)=>{
+
+})
+app.post('/api/v1/content' , (req,res)=>{
+
+})
+app.get('/api/v1/content' , (req,res)=>{
+
+})
+app.delete('/api/v1/content' , (req,res)=>{
+
+})
+app.post('/api/v1/share' , (req,res)=>{
+
+})
+app.get('/api/v1/signup' , (req,res)=>{
+
+})
